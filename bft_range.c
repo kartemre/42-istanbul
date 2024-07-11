1fe147da61fe4724373e450e7e31b811a8620d7c
Emre Kart <76533719+kartemre@users.noreply.github.com>
2024-07-11 11:05:15 +0300

    Add files via upload

--- bft_range.c
+++ bft_range.c
@@ -1,4 +1,4 @@
-/*
+
 Assignment name  : ft_range
 Expected files   : ft_range.c
 Allowed functions: malloc
@@ -19,52 +19,16 @@ Examples:
 - With (0, 0) you will return an array containing 0.
 - With (0, -3) you will return an array containing 0, -1, -2 and -3.
 */
-#include <stdlib.h>
-int     *ft_range(int min, int max)
+int     *ft_range(int start, int end)
 {
 	int range;
 	int *d;
 	int *dest;
-	int i;
 
-	if (max - min < 0)
-		range = min - max + 1;
-	else
-		range = max - min + 1;
-	d = ((dest = (int *)malloc(range * sizeof(int))));
+	range = end - start;
+	d = ((dest = (char *)malloc(range * sizeof(char)));
 	if(!d)
 		return 0;
-	if (max > min)
-	{
-		while (i < range)
-		{
-			d[i] = min + i; 
-			i++;
-		}
-	}
-	else if (min > max)
-	{
-		while (i < range)
-		{
-			d[i] = min; 
-			i++;
-			min--;
-		}
-	}
-	return (dest);	
-}
-#include <stdio.h>
-int main()
-{
-	int min = 9,max = -1,size, i = 0;
-	int *x = ft_range(min,max);
-	size  = min - max;
-	if (size < 0)
-		size *= -1;
-	while (i <= size)
-	{
-		printf("%d", x[i]);
-		i++;
-	}
+	
 	
 }
